cbedf81243be60617cd99d4344f34f7adcaee7dc
crspytopgn <uktopgun@gmail.com>
2026-01-26 13:15:09 +0000

    Update __init__.py

--- custom_components/railboard/__init__.py
+++ custom_components/railboard/__init__.py
@@ -1,13 +1,1 @@
 """Railboard integration."""
-
-import logging
-from homeassistant.helpers.discovery import load_platform
-from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
-def setup(hass, config):
-    """Set up the Railboard integration."""
-    _LOGGER.warning("🔥 RAILBOARD SETUP CALLED 🔥")
-    load_platform(hass, "sensor", DOMAIN, {}, config)
-    return True
