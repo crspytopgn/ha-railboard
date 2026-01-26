3a8eec7fc181426a39c54f58bc3f10b3f1ae28ca
crspytopgn <uktopgun@gmail.com>
2026-01-26 11:22:41 +0000

    Update sensor.py

--- custom_components/railboard/sensor.py
+++ custom_components/railboard/sensor.py
@@ -1,1 +1,20 @@
-# Sensor logic will live here
+"""Sensor platform for Railboard."""
+
+from homeassistant.helpers.entity import Entity
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Railboard sensor."""
+    add_entities([RailboardDummySensor()])
+
+
+class RailboardDummySensor(Entity):
+    """A dummy sensor to test Railboard integration."""
+
+    def __init__(self):
+        self._attr_name = "Railboard Test Sensor"
+        self._state = "OK"
+
+    @property
+    def state(self):
+        return self._state
