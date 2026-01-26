c1a74600e8dd08d38606eb34fae432cd14fd51c8
crspytopgn <uktopgun@gmail.com>
2026-01-26 12:31:35 +0000

    Update sensor.py

--- custom_components/railboard/sensor.py
+++ custom_components/railboard/sensor.py
@@ -1,20 +1,25 @@
-"""Sensor platform for Railboard."""
-
 from homeassistant.helpers.entity import Entity
+from .api import DarwinClient
 
+darwin = DarwinClient()
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the Railboard sensor."""
-    add_entities([RailboardDummySensor()])
+    add_entities([RailboardDeparturesSensor()])
 
-
-class RailboardDummySensor(Entity):
-    """A dummy sensor to test Railboard integration."""
-
+class RailboardDeparturesSensor(Entity):
     def __init__(self):
-        self._attr_name = "Railboard Test Sensor"
-        self._state = "OK"
+        self._attr_name = "Railboard Departures"
+        self._state = None
+        self._attr_extra_state_attributes = {}
 
     @property
     def state(self):
         return self._state
+
+    def update(self):
+        # This is called by HA to refresh the sensor
+        departures = darwin.get_departures("KGX")  # example CRS
+        self._state = len(departures)  # number of departures
+        self._attr_extra_state_attributes = {
+            f"{i+1}": dep for i, dep in enumerate(departures)
+        }
