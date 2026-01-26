c728e5256db0881f14f583bcb46195014a98929f
crspytopgn <uktopgun@gmail.com>
2026-01-26 11:03:56 +0000

    Update __init__.py

--- custom_components/railboard/__init__.py
+++ custom_components/railboard/__init__.py
@@ -1,1 +1,10 @@
-# Railboard integration
+"""Railboard integration."""
+
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+
+def setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the Railboard integration."""
+    hass.data[DOMAIN] = {}
+    return True
