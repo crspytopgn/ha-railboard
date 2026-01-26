5520f1958de74c6cefe82b33ae83346291787d72
crspytopgn <uktopgun@gmail.com>
2026-01-26 14:26:45 +0000

    Update __init__.py

--- custom_components/railboard/__init__.py
+++ custom_components/railboard/__init__.py
@@ -1,1 +1,12 @@
-"""Railboard integration."""
+"""The Railboard integration."""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "railboard"
+
+
+def setup(hass, config):
+    """Set up the Railboard component."""
+    _LOGGER.info("Setting up Railboard integration")
+    return True
