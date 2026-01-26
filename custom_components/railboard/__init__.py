d6e1fd1387aaccf0fa406fd21d8d31a20e0fd49c
crspytopgn <uktopgun@gmail.com>
2026-01-26 11:23:02 +0000

    Update __init__.py

--- custom_components/railboard/__init__.py
+++ custom_components/railboard/__init__.py
@@ -1,10 +1,12 @@
 """Railboard integration."""
 
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.discovery import load_platform
 from .const import DOMAIN
 
 
 def setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the Railboard integration."""
     hass.data[DOMAIN] = {}
+    load_platform(hass, "sensor", DOMAIN, {}, config)
     return True
