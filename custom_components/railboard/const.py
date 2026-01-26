f7912d324642a97188322e53ba3ff2bc63e16732
crspytopgn <uktopgun@gmail.com>
2026-01-26 14:36:07 +0000

    Update const.py

--- custom_components/railboard/const.py
+++ custom_components/railboard/const.py
@@ -1,2 +1,22 @@
+"""Constants for the Railboard integration."""
+
 DOMAIN = "railboard"
-DARWIN_WSDL = "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl/ldb3.wsdl"
+
+# Configuration
+CONF_STATION_CODE = "station_code"
+CONF_STATION_NAME = "station_name"
+CONF_RTT_USERNAME = "rtt_username"
+CONF_SHOW_ARRIVALS = "show_arrivals"
+CONF_MAX_RESULTS = "max_results"
+CONF_SHOW_PLATFORMS = "show_platforms"
+CONF_SHOW_STATUS = "show_status"
+CONF_SHOW_CALLING_POINTS = "show_calling_points"
+CONF_SHOW_OPERATOR_BADGE = "show_operator_badge"
+
+# Defaults
+DEFAULT_MAX_RESULTS = 15
+DEFAULT_SHOW_ARRIVALS = False
+DEFAULT_SHOW_PLATFORMS = True
+DEFAULT_SHOW_STATUS = True
+DEFAULT_SHOW_CALLING_POINTS = True
+DEFAULT_SHOW_OPERATOR_BADGE = True
